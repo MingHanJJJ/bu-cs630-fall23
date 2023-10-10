3fb31d4b1f03d80b1278893de4a562e52ae4a574
Ming-Han Hsieh <jeffhsieh0987@gmail.com>
2023-10-10 11:08:56 -0400

    hw2 finished

--- hw2/coding.py
+++ hw2/coding.py
@@ -5,4 +5,33 @@ def best_position(points, weights):
 
   Return: a tuple (x,y) that represents the location of the point that minimizes the weighted distance sum.
   '''
+
+  # find weight median x
+  xs = list(zip(list(zip(*points))[0], weights))
+  ys = list(zip(list(zip(*points))[1], weights))
+  xs.sort(key=lambda x: x[0])
+  ys.sort(key=lambda x: x[0])
+
+  w_m_x = 0
+  w_m_y = 0
+  Sum1 = xs[0][1]
+  Sum2 = 0
   
+  for i in range(len(xs)-1):
+    Sum1 += xs[i+1][1]
+    Sum2 += xs[i][1]
+    if(Sum1 >= 0.5 and Sum2 < 0.5):
+      w_m_x = xs[i+1][0]
+      break
+
+  Sum1 = ys[0][1]
+  Sum2 = 0
+  
+  for i in range(len(ys)-1):
+    Sum1 += ys[i+1][1]
+    Sum2 += ys[i][1]
+    if(Sum1 >= 0.5 and Sum2 < 0.5):
+      w_m_y = ys[i+1][0]
+      break
+
+  return (w_m_x, w_m_y)
---
+++ hw2/mian.py
@@ -0,0 +1,9 @@
+import coding
+  
+# Defining main function 
+def main(): 
+    ans = coding.best_position([(0.2, 0.1), (0.1, 0.2), (0.3, 0.4), (0.5, 0.1), (0.6, 0.7)], [0.1, 0.1, 0.1, 0.3, 0.4])
+    print(ans)
+  
+if __name__=="__main__": 
+    main() 
